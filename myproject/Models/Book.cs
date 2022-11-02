7b21e469877ffa963b11d3a5fbaf5987a034d074
GOODLUCK <GOODLUCK@DESKTOP-KONPHJ7>
2022-11-01 17:08:12 -0700

    Add project files.

---
+++ myproject/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace myproject.Models
+{
+    public class Book
+    {
+        [JsonPropertyName("id")]
+        public int Book_id { get; set; }
+        [JsonPropertyName("name")]
+        public string Book_name { get; set; }
+        [JsonPropertyName("image")]
+        public string Book_image { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Book>(this);
+        }
+    }
+}
